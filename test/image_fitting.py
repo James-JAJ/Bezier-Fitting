14c62b483e37f84eed6a9c65fdc510af0905831a
JamesJAJ <JAJ.games,player@gmail.com>
2025-06-02 15:59:22 +0800

    modified:   test/image_fitting.py 	modified:   "\345\267\245\344\275\234\346\233\270.docx"

--- test/image_fitting.py
+++ test/image_fitting.py
@@ -4,7 +4,6 @@ import sys
 import os
 import cv2
 from PIL import Image
-import svgwrite
 
 sys.stdout.reconfigure(encoding='utf-8')
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
